821cf22ec08419ca97c2b5e97389665551559bbf
shb190802 <609690891@qq.com>
2019-06-14 18:16:40 +0800

    'tijiao'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'VueJS 前端开发规范',
   description: 'VueJS 前端开发规范',
-  base: 'https://shb190802.github.io/front-end-dev-guide/dist/',
+  base: '/front-end-dev-guide/dist/',
   dest: 'dist',
   themeConfig: {
     sidebar: [
